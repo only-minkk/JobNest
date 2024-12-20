0e6c51f4f8bb5a0383fd31392e4d0bf48b855518
minkyu <tour00389@gmail.com>
2024-12-20 17:33:50 +0900

    Initialize project with pakcage.json and index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+// import dotenv from "dotenv";
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("JobNest 프로젝트 서버 시작! (ES6)");
+});
+
+app.listen(PORT, () => {
+  console.log(`서버가 http://localhost:${PORT}에서 실행 중입니다.`);
+});
